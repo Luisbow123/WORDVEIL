7250ac3ffd2b6d5a53840e3badb65f9099ea4702
Joshua Galvez <jgalv049@fiu.edu>
2023-07-14 10:11:50 -0400

    Fixing popup.js logic

--- popup.js
+++ popup.js
@@ -1,45 +1,44 @@
 //Array for user's words to block
 let blockedWords = [];
+let blockedWordsCount;
 
-//This logic will check if their input is a duplicate word, then add it, if not, ignore it:
-let input = document.getElementsByName("myButton").onclick = function(){
+/*/ This logic will check if their input is a duplicate word, then add it, if not, ignore it:
+      [Assuming (value = "string" checks for the same word using case sensitive stuff i think)] /*/
+document.getElementById("getWord").onclick = function(){
 
-blockedWordsCount = 0;
-let val = true;
-
-for (n = 0; n++; n<=blockedWords.length)
-
-if (val = true){
+//If newWord stays true throughout function, the function ends (if false, duplicate detected/while loop exited)
+let newWord = true;
+while (newWord == true){
 
   for (n = 0; n++; n<=blockedWords.length){
 
+    if (document.getElementById("userInput").value.equals == blockedWords[n]) {
 
-
-    if (document.getElementById("getWord").value = blockedWords[n]) {
-
+      alert ("Duplicate word detected");  //User is alerted of duplicate word (case sensitive i think)
+      n = blockedWords.length + 2;  //I make n > blockedWords.length to jump out of forLoop
+      newWord = false;
 
 
  }  else {
 
-        blockedWords[blockedWordsCount] = document.getElementById("userInput").value;
-        blockedWordsCount++;
+  if (n == blockedWords.length){
+
+
+    blockedWords[blockedWordsCount] = document.getElementById("userInput").value;
+    blockedWordsCount++;
+
+    newWord = false;
+
+  }
+}
 
  }
 
+} //forLoop loops here
 }
-}else {
-
-
-
-}
-}
-
-
 
 document.getElementById("getWord").onclick = function(){
-    let wordInputed = document.getElementById("userInput").value;
-    console.log("Hello",wordInputed);
+  let wordInputed = document.getElementById("userInput").value;
+  console.log("Hello",wordInputed);
 }
-
-
 
